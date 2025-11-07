593ba3aded11c9cbff67cdbc154f03e0bbaad84b
JR014 <124470232+JairoRaudales@users.noreply.github.com>
2025-11-07 10:46:00 -0600

    Delete config directory

--- config/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class LoanRules(models.Model):
-    plazo_dias = models.PositiveIntegerField(default=7)
-    max_prestamos_por_cliente = models.PositiveIntegerField(default=3)
-    mora_diaria = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
-    def __str__(self):
-        return f"Reglas(plazo={self.plazo_dias}, max={self.max_prestamos_por_cliente}, mora={self.mora_diaria})"
