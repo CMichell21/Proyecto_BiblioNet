2cd2831b52c1a444d18fff1878192d43b48a7075
CMichell21 <nancycarrillo200321@gmail.com>
2025-11-16 21:22:46 -0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Alias para que los tests encuentren estos nombres 
+    path("login/", seguridad_views.iniciar_sesion_empleado, name="login-page"),
+    path("logout/", seguridad_views.cerrar_sesion_empleado, name="logout-page"),
+    path("mi-cuenta/", seguridad_views.panel_administrador, name="mi-cuenta"),
+
     path("", include("biblio.urls")),      # Inicio, catálogo, login cliente…
     path("", include("seguridad.urls")),   # Login empleados y panel admin
 ]
+
