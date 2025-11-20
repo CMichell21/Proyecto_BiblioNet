fd8fcce0842edaaed965ed19ee7e35e69378510d
eddmastr7 <145728366+eddmastr7@users.noreply.github.com>
2025-11-20 16:32:14 -0600

    Update urls.py
    URLS UPDATES PRESTAMOS

--- seguridad/urls.py
+++ seguridad/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path("pantalla_inicio/bibliotecario/", views.panel_bibliotecario, name="panel_bibliotecario"),
     path("empleados/registrar/", views.registrar_empleado, name="registrar_empleado"),
     path("empleados/inventario/",views.inventario, name="inventario"),
-    
+    path("reglas-prestamo/configuracion/",views.configurar_reglas_prestamo, name="configurar_reglas_prestamo",),
+
+    path("prestamos/gestion/", views.gestion_prestamos, name="gestion_prestamos"),
+    path("prestamos/registrar/", views.registrar_prestamo, name="registrar_prestamo"),
+    path("prestamos/<int:prestamo_id>/devolver/", views.devolver_prestamo, name="devolver_prestamo"),
+    path("prestamos/<int:prestamo_id>/renovar/", views.renovar_prestamo, name="renovar_prestamo"),
 ]
